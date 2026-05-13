05e5f4f0c8ea12f2dfc5d39511389a1fd9424104
Алексей Мешечко <alekseymeshechko@Mac-mini-Aleksej.local>
2026-05-13 18:22:27 +0300

    Add template inheritance and product detail page

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product, Contact
 
 
-
 def home(request):
-    products = Product.objects.order_by('-created_at')[:5]
-
-    print(products)
+    products = Product.objects.all()
 
     context = {
         'products': products
     }
 
     return render(request, 'catalog/home.html', context)
-
 
 
 def contacts(request):
@@ -25,3 +21,13 @@ def contacts(request):
     }
 
     return render(request, 'catalog/contacts.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'catalog/product_detail.html', context)
