c4ce7c8c94cf89ca8d290907080a12f55f24bcfe
Алексей Мешечко <alekseymeshechko@Mac-mini-Aleksej.local>
2026-05-20 21:48:36 +0300

    Add Redis caching and category products service

--- config/settings.py
+++ config/settings.py
@@ -218,3 +218,18 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # CUSTOM ERROR PAGES
 
 CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
+
+
+# CACHE / REDIS
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+    }
+}
